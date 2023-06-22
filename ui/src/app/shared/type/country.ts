06f39f094861f62ae951bc8c5a130d756d059797
Alexander Stoyanov <30179510+avstoyanov@users.noreply.github.com>
2023-06-22 06:01:38 -0400

    added fields for sign up form, as well as errors in the en.json file. Added 2 new countries to country.ts: usa and bulgaria. TODO: fix sign-up form, implement new countries where necessary, and add newly added fields to all other language files.

--- ui/src/app/shared/type/country.ts
+++ ui/src/app/shared/type/country.ts
@@ -7,6 +7,8 @@ export enum Country {
     SWEDEN = 'se',
     CZECH_REPUBLIK = 'cz',
     NETHERLANDS = 'nl',
+    UNITED_STATES = 'us',
+    BULGARIA = 'bg'
 }
 
 export const COUNTRY_OPTIONS = (translate: TranslateService) => {
@@ -16,6 +18,8 @@ export const COUNTRY_OPTIONS = (translate: TranslateService) => {
         { value: Country.SWITZERLAND, label: translate.instant('General.Country.switzerland') },
         { value: Country.SWEDEN, label: translate.instant('General.Country.sweden') },
         { value: Country.NETHERLANDS, label: translate.instant('General.Country.netherlands') },
-        { value: Country.CZECH_REPUBLIK, label: translate.instant('General.Country.czech') }
+        { value: Country.CZECH_REPUBLIK, label: translate.instant('General.Country.czech') },
+        { value: Country.UNITED_STATES, label: translate.instant('General.Country.usa') }, 
+        { value: Country.BULGARIA, label: translate.instant('General.Country.bulgaria') }
     ];
 };
