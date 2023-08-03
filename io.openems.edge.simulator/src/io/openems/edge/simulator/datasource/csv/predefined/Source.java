c90c954226ee5dd2457a155514e11d91b5ce3c02
Alexander Stoyanov <30179510+avstoyanov@users.noreply.github.com>
2023-08-03 09:26:52 -0400

    added wind simulation predefined csv

--- io.openems.edge.simulator/src/io/openems/edge/simulator/datasource/csv/predefined/Source.java
+++ io.openems.edge.simulator/src/io/openems/edge/simulator/datasource/csv/predefined/Source.java
@@ -2,6 +2,7 @@ package io.openems.edge.simulator.datasource.csv.predefined;
 
 public enum Source {
 	ZERO("zero.csv"), //
+	WIND_SIM("power_sim.csv"), //
 	H0_HOUSEHOLD_SUMMER_WEEKDAY_STANDARD_LOAD_PROFILE("h0-summer-weekday-standard-load-profile.csv"), //
 	H0_HOUSEHOLD_SUMMER_WEEKDAY_PV_PRODUCTION("h0-summer-weekday-pv-production.csv"), //
 	H0_HOUSEHOLD_SUMMER_WEEKDAY_NON_REGULATED_CONSUMPTION("h0-summer-weekday-non-regulated-consumption.csv"), //
